3b2f901ea4bfc36369d6ae216d7ee9640915b397
Bergson <bergsonsud@gmail.com>
2017-05-28 03:07:34 -0300

    Test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,10 @@
 class HomeController < ApplicationController
 before_action :set_status
-#before_action :authenticate, except: [ :index ]
 
 require 'open-uri'
 require 'rest-client'
 require 'rubygems'
 require 'json'
-require 'pp'
 
 
   def index 
@@ -20,27 +18,20 @@ require 'pp'
 	@teams = @teams.with_indifferent_access	
   end
 
+  def minha_liga
+  	@teams = @ads[:times]
+  end
+
 
 private
     def set_status
       @status = JSON.parse(open("https://api.cartolafc.globo.com/mercado/status").read, symbolize_names: true)  	
       jdata =  JSON.parse('{"payload":{"email":"bergsonsud@gmail.com","password":"","serviceId": 438}}')  		                
       #@request = RestClient.post('https://login.globo.com/api/authentication', jdata)
-      #@request = RestClient.post('https://login.globo.com/api/authentication', jdata.to_json, content_type: :json)
-      @ads = JSON.parse(open("https://api.cartolafc.globo.com/ligas?q=adsfn2013.2").read, symbolize_names: true)      
-
-
-
+      @request = RestClient.post('https://login.globo.com/api/authentication', jdata.to_json, content_type: :json)
+      #@aads = JSON.parse(open("https://api.cartolafc.globo.com/ligas?q=adsfn2013.2").read, symbolize_names: true)      
+      token = '1f78a2a43061262a4ca86b1fe12ba3fb12d38656a7571757a73715145614b6774567a65476c466647596c354736624568456d435a324257574e5475773669566865416c6e7547526a38646e4b6865477968436d65346c667a7a55324d4a5a61486130715435413d3d3a303a62657267736f6e7375645f6c696d61'
+      @ads = JSON.parse(RestClient.get("https://api.cartolafc.globo.com/auth/liga/adsfn2013-2", {'X-GLB-Token' => token}), symbolize_names: true)
     end  
-
-	def authenticate
-	  authenticate_or_request_with_http_token do |token, options|
-	    # Compare the tokens in a time-constant manner, to mitigate
-	    # timing attacks.
-	    ActiveSupport::SecurityUtils.secure_compare(
-	      ::Digest::SHA256.hexdigest(token),
-	      ::Digest::SHA256.hexdigest(TOKEN)
-	    )
-	  end
-	end    
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do  
-  root 'home#index'
-  get 'home/actual_games'
+  get 'home#index'
+  root 'home/actual_games'
+  get 'home/minha_liga'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
