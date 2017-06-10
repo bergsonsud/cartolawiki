07f579eeccaf90088bebf84f918c419f97ad9795
Bergson Lima <bergsonsud@gmail.com>
2017-06-10 13:34:56 -0300

    Minha liga ajustes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -35,8 +35,7 @@ private
 
       token = '1f78a2a43061262a4ca86b1fe12ba3fb12d38656a7571757a73715145614b6774567a65476c466647596c354736624568456d435a324257574e5475773669566865416c6e7547526a38646e4b6865477968436d65346c667a7a55324d4a5a61486130715435413d3d3a303a62657267736f6e7375645f6c696d61'      
         RestClient.get("https://api.cartolafc.globo.com/auth/liga/adsfn2013-2", {'X-GLB-Token' => token}) do |f|        
-          x = JSON.parse(f, symbolize_names: true)
-          
+          x = JSON.parse(f, symbolize_names: true)          
           if x[:mensagem].to_s != "Mercado em manutenção."
             @ads = JSON.parse(RestClient.get("https://api.cartolafc.globo.com/auth/liga/adsfn2013-2", {'X-GLB-Token' => token}), symbolize_names: true)
           else
@@ -59,11 +58,20 @@ private
 
     def set_status     
       @status = JSON.parse(open("https://api.cartolafc.globo.com/mercado/status").read, symbolize_names: true)          
+      @mercado = definir_mercado(@status[:status_mercado].to_i)
     end  
 
+      def definir_mercado status
+        case status
+        when 1
+          "Aberto"
+        else
+          "Fechado"
+        end
+      end
+
     def set_status_liga  
-      status
-      
+      status      
     end      
  
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,16 +26,22 @@ module ApplicationHelper
 		rodada >0 and rodada <atual ? true : false		
 	end
 
+	def atletas_por_slug slug
+		atletas = JSON.parse(RestClient.get("https://api.cartolafc.globo.com/time/slug/"+slug, nil), symbolize_names: true)
+	end
 
 	def somar_pontos_por_slug slug
 		#token = '1f78a2a43061262a4ca86b1fe12ba3fb12d38656a7571757a73715145614b6774567a65476c466647596c354736624568456d435a324257574e5475773669566865416c6e7547526a38646e4b6865477968436d65346c667a7a55324d4a5a61486130715435413d3d3a303a62657267736f6e7375645f6c696d61'      	
-      	points = JSON.parse(RestClient.get("https://api.cartolafc.globo.com/atletas/pontuados", nil), symbolize_names: true)
-      	points = points[:atletas].with_indifferent_access
-      	atletas = JSON.parse(RestClient.get("https://api.cartolafc.globo.com/time/slug/"+slug, nil), symbolize_names: true)
-      	pontos  = 0
-      	atletas[:atletas].each do |atleta|      		
-      		#puts points[atleta[:atleta_id].to_s][:pontuacao].to_s
-      	end
-      	0
+	      if @mercado != "Aberto"
+	      	points = JSON.parse(RestClient.get("https://api.cartolafc.globo.com/atletas/pontuados", nil), symbolize_names: true)
+	      	points = points[:atletas].with_indifferent_access
+	      	atletas = JSON.parse(RestClient.get("https://api.cartolafc.globo.com/time/slug/"+slug, nil), symbolize_names: true)
+	      	pontos  = 0      	
+	      	atletas[:atletas].each do |atleta|      		
+	      		#puts points[atleta[:atleta_id].to_s][:pontuacao].to_s
+	      	end
+	      else
+	      	0
+	      end
 	end
 end
